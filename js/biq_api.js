609268bf1db340cba4fc7a8f0bf0d909bdea6fe1
Ollie <ucasolusteveo@gmail.com>
2023-09-28 19:24:36 +0100

    created login with Taxicode

--- js/biq_api.js
+++ js/biq_api.js
@@ -12,5 +12,24 @@ var BIQ = {
             success: success,
             dataType: "json"
           });
+    },
+    "login" : function(data, success) {
+        var url = BIQ.api_host + "user/login/";
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: data,
+            success: success,
+            dataType: "json"
+          });
+    },
+    "logout" : function(success) {
+        var url = BIQ.api_host + "user/logout/";
+        $.ajax({
+            type: "GET",
+            url: url,
+            success: success,
+            dataType: "json"
+        });
     }
 }
--- js/main.js
+++ js/main.js
@@ -13,9 +13,42 @@ function quoteCard(quote_id, quote_data) {
     return "<div id=\"quote-" + quote_id + "\"><h3>" + quote_id + ": - " + quote_data.company_name + " &pound;" + parseFloat(quote_data.vehicles[0].price).toFixed(2) + "</h3></div>"
 }
 
+function showLogout() {
+    $("#logout-button-container").show();
+}
+
+function hideLogout() {
+    $("#logout-button-container").hide();
+}
+
+function showLogin() {
+    $("#login-button-container").show();
+}
+
+function hideLoginButton() {
+    $("#login-button-container").hide();
+}
+
+function hideLogin() {
+    $("#login-form-container").hide();
+}
+
+
+function userLoggedIn(tc_user_auth){
+    showLogout();
+    $("#login-button-container").hide();
+}
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("event loaded")
+    console.log("event loaded");
+    token = Cookie.get('tc_user_auth');
+    console.log(token);
+    if (token) {
+        userLoggedIn(token);
+    } else {
+        showLogin();
+    }
     $( "#date" ).datepicker();
     $('#time').timepicker({
         timeFormat: 'h:mm p',
@@ -26,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         scrollbar: true
     });
     $("#biq-journey-form-submit").on("click", function(e){
-        e.preventDefault()
+        e.preventDefault();
         var f = $("#biq-journey-form"),
             j = {
                 "pickup" : f.find("[name=pickup]").val(),
@@ -56,5 +89,42 @@ document.addEventListener("DOMContentLoaded", function(event) {
             console.log(response)
         })
         return false
+    });
+    $("#show-login-form").on("click", function(e) {
+        $("#login-form-container").show();
+        hideLoginButton();
+    });
+    $("#login-form-submit").on("click", function(e){
+        e.preventDefault()
+        var f = $("#login-form");
+        BIQ.login({
+            "email" : f.find("[name='login']").val(),
+            "password" : f.find("[name='password']").val()
+        }, function(response) {
+            console.log(response);
+            if(response.status == BIQ.STATUS_OK) {
+                Cookie.set("tc_user_auth", response.user.auth_token, 30);
+                userLoggedIn(response.user.auth_token);
+                hideLogin();
+                showLogout();
+            } else {
+                alert("Unable to Login")
+            }
+        });
+
+    $("#cancel-login").on("click", function(e){
+        e.preventDefault();
+        hideLogin();
     })
+    });
+    $("#logout").on("click", function(e){
+        e.preventDefault()
+        BIQ.logout(function(response) {
+            console.log(response);
+            Cookie.remove("tc_user_auth");
+            hideLogout();
+            showLogin();
+        })
+
+    });
 })
