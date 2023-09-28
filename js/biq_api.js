95083dbee2e5afc4cad7253ed2798fa93302d80f
Ollie <ucasolusteveo@gmail.com>
2023-09-28 15:09:03 +0100

    initial commit of html & js

--- js/biq_api.js
+++ js/biq_api.js
@@ -1,14 +1,16 @@
+var BIQ = {
+    STATUS_OK : "OK",
+    STATUS_ERROR : "ERROR",
+    "api_host" : "https://api.taxicode.com/",
 
-var api_host = "https://api.taxicode.com/";
-
-
-function getQuotes(data, success){
-    var url = api_host + "booking/quote/"
-    $.ajax({
-        type: "GET",
-        url: url,
-        data: data,
-        success: success,
-        dataType: "json"
-      });
+    "getQuotes" : function(data, success) {
+        var url = BIQ.api_host + "booking/quote/"
+        $.ajax({
+            type: "GET",
+            url: url,
+            data: data,
+            success: success,
+            dataType: "json"
+          });
+    }
 }
--- js/main.js
+++ js/main.js
@@ -4,27 +4,57 @@
 
 var journey = {
     "pickup" : "York",
-    "vias" : "",
     "destination" : "York Station",
     "date" : "03-09-2024 10:30:34",
     "passengers" : "2"
 }
 
-
-
-
-
-
-console.log("event")
-
-
-
+function quoteCard(quote_id, quote_data) {
+    return "<div id=\"quote-" + quote_id + "\"><h3>" + quote_id + ": - " + quote_data.company_name + " &pound;" + parseFloat(quote_data.vehicles[0].price).toFixed(2) + "</h3></div>"
+}
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("event loaded")
-    console.log(getQuotes(journey, function(response){
-        console.log(response)
-    }))
+    $( "#date" ).datepicker();
+    $('#time').timepicker({
+        timeFormat: 'h:mm p',
+        defaultTime: '11',
+        interval: 5,
+        dynamic: true,
+        dropdown: true,
+        scrollbar: true
+    });
+    $("#biq-journey-form-submit").on("click", function(e){
+        e.preventDefault()
+        var f = $("#biq-journey-form"),
+            j = {
+                "pickup" : f.find("[name=pickup]").val(),
+                "destination" : f.find("[name=destination]").val(),
+                "date" : f.find("[name=date]").val() + f.find("[name=time]").val(),
+                "passengers" : parseInt(f.find("[name=passengers]").val())
+            };
+            console.log(j)
+            console.log(f)
+        BIQ.getQuotes(j, function(response){
+            if(response.status == BIQ.STATUS_OK) {
+                if (Object.keys(response.quotes).length) {
 
+                    var ele = $("<div></div>")
+                    $.each(response.quotes, function(idx, itm) {
+                        console.log(itm)
+                        ele.append(quoteCard(idx, itm))
+        
+                    })
+                } else {
+                    var ele = $("<h4>" + response.warnings[0] + "</h4>")
+                }
+                $("#quote-results").html(ele)
+            } else {
+                $("#quote-results").html($("<h4>" + response.error + "</h4>"))
+            }
+            console.log(response)
+        })
+        return false
+    })
 })
